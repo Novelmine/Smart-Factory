7a92f30521c56d525903b9e7b438e9ae81e990bd
Novelmine <110361118+Novelmine@users.noreply.github.com>
2022-09-16 09:25:59 +0900

    Add files via upload

---
+++ node/1단계/ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = 0;
+            while (true)
+            {
+                StreamWriter writer;
+                writer = File.AppendText("C:\\Users\\Admin\\Desktop\\writeTest.txt");
+                for (int i = 0; i < 10; i++)
+                {
+                    writer.WriteLine(DateTime.Now);
+                }
+                writer.Close();
+                Console.WriteLine(p);
+                p++;
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
