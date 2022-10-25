0a9c2a2c9b3b79ed3ebe04ff01980f1f887ee359
Novelmine <110361118+Novelmine@users.noreply.github.com>
2022-10-25 15:41:04 +0900

    Add files via upload

---
+++ SOLID/pattern/Observer Pattern/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WeatherStation.Observable.Observers;
+using WeatherStation.Observable.Subjects;
+
+namespace WeatherStation.Observable.Observers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WeatherData weatherData = new WeatherData();
+
+            CurrentConditionsDisplay currentDisplay = new CurrentConditionsDisplay(weatherData);
+            StatisticsDisplay statisticsDisplay = new StatisticsDisplay(weatherData);
+            ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+            HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
+
+            weatherData.SetMeasurements(80, 65, 30.4d);
+            weatherData.SetMeasurements(82, 70, 29.2d);
+            weatherData.SetMeasurements(78, 90, 29.2d);
+        }
+    }
+
+}
